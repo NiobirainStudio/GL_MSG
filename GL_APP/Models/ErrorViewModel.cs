5e99fd1ada40259a7e5402994e7de782228ce71b
NidvenQ <niobirain@gmail.com>
2022-01-21 21:30:08 +0200

    Project redone

---
+++ GL_APP/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GL_APP.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
