a1b4ecf47e1bdfec112ec1430172bc256be4a417
Vsevolod Zhdanov <zhdanoov777@gmail.com>
2022-02-23 14:44:32 +0200

    resolved migration issue

---
+++ GL_PROJ/Migrations/20220223124316_DBExpansion2.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GL_PROJ.Migrations
+{
+    public partial class DBExpansion2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_IconId",
+                table: "Users",
+                column: "IconId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_Icons_IconId",
+                table: "Users",
+                column: "IconId",
+                principalTable: "Icons",
+                principalColumn: "IconId",
+                onDelete: ReferentialAction.SetDefault);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_Icons_IconId",
+                table: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Users_IconId",
+                table: "Users");
+        }
+    }
+}
